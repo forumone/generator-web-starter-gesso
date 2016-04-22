d6075839039af05ac8ee9b58af61f59304586d83
Thibaut Lassalle <thibaut.lassalle@gmail.com>
2016-04-22 14:52:11 -0400

    add gesso theme for drupal

--- web-starter/index.js
+++ web-starter/index.js
@@ -6,26 +6,62 @@ var generators = require('yeoman-generator'),
   semver = require('semver'),
   libxmljs = require("libxmljs"),
   request = require("request"),
-  glob = Promise.promisify(require('glob'));
+  glob = Promise.promisify(require('glob')),
+  http = require('http'),
+  fs = require('fs'),
+  Download = require('download');
 
-var gessoLastVerion = "7.x-1.4";
+var DRUPAL_GESSO_URL = "https://updates.drupal.org/release-history/gesso/all";
 
 module.exports = generators.Base.extend({
   initializing : {
     version : function() {
-      request({url : "https://updates.drupal.org/release-history/gesso/all"}, function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-          gessoLastVerion = libxmljs.parseXml(body).get('//release/version').text();
-        }
-        else {
-          console.log('WARNING: you may not use the last version of gesso!');
-        }
-      });
+      if(!this.options.parent) {
+        //for test purposes 
+        this.options.parent = {};
+        this.options.parent.answers = {};
+        this.options.parent.answers.platform = 'drupal';
+      }
+      var that = this;
+      if(this.options.parent.answers.platform === 'drupal') {
+        request({url : DRUPAL_GESSO_URL}, function(error, response, body) {
+          if (!error && response.statusCode === 200) {
+            that.config.gessoDl = libxmljs.parseXml(body).get('//release/download_link').text();
+          }
+          else {
+            console.log('ERROR unable to retrieve gesso version number from Drupal: ' + DRUPAL_GESSO_URL);
+          }
+        });
+      }
+      else if (this.options.parent.answers.platform === 'wordpress') {
+        //TODO https://github.com/forumone/gesso-theme-wordpress
+      }
     }
+  },
+  prompting : function() {
+    var done = this.async();
+    var that = this;
+    that.prompt([{
+      type: 'confirm',
+      name: 'install_gesso',
+      message: 'Install a fresh copy of the gesso theme?',
+      default: false,
+    }], function (answers) {
+      _.extend(that.config, answers);
+      done();
+    });
   },
   writing : {
     settings : function() {
-      console.log("gesso last version: " + gessoLastVerion);
+      if(this.options.parent.answers.platform === 'drupal') {
+        if(this.config.install_gesso) {
+          // https://github.com/kevva/download
+          new Download({extract: true}).get(this.config.gessoDl, 'public/themes/').dest('.').run();
+        }
+      }
+      else if (this.options.parent.answers.platform === 'wordpress') {
+        //TODO 
+      }
     }
   }
 });
