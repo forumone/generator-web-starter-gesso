6e21de3c325af0623989ffe22f861e96a8d76c16
William Hurley <whurley@forumone.com>
2016-06-13 22:21:03 -0400

    Changing sass globbing filename

--- web-starter/templates/tasks/sass/sass_globbing.js
+++ web-starter/templates/tasks/sass/sass_globbing.js
@@ -1,7 +1,7 @@
 {
       gesso : {
         files : (function(grunt) {
-          var opts = grunt.file.readJSON(grunt.config.get('pkg').themePath + '/sass/globs.json');
+          var opts = grunt.file.readJSON(grunt.config.get('pkg').themePath + '/sass/sass-globbing.json');
           return Object.keys(opts).reduce(function(previous, current) {
             previous[grunt.config.get('pkg').themePath + '/sass/' + current] = grunt.config.get('pkg').themePath + '/sass/' + opts[current];
             return previous;
