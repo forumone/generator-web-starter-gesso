f5352e4269034da9f16776b4784f773bd71695b3
Andrew Morton <amorton@forumone.com>
2017-02-21 12:47:28 -0800

    20: Remove drupal-modules include

--- web-starter/index.js
+++ web-starter/index.js
@@ -5,10 +5,7 @@ var generators = require('yeoman-generator'),
   glob = Promise.promisify(require('glob')),
   fs = require('fs'),
   pkg = require('../package.json'),
-  ygp = require('yeoman-generator-bluebird'),
-  drupal_modules = require('drupal-modules');
-
-var DRUPAL_GESSO_URL = "https://updates.drupal.org/release-history/gesso/all";
+  ygp = require('yeoman-generator-bluebird');
 
 module.exports = generators.Base.extend({
   initializing : {
